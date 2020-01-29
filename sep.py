26aa64c1736fc64dedd540c8f9b6ee7a4dda0cd0
Cappuccinomild <rkdquddh1208@naver.com>
2020-01-29 01:25:46 +0000

    elbow

--- sep.py
+++ sep.py
@@ -3,12 +3,13 @@ import cv2
 import numpy as np
 import random
 import copy
- 
+import matplotlib.pyplot as plt
+
 def heuristic(a, b):
 	return (b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2
 
 def astar(array, start, goal):
-	
+
 	start = (int(start[0]), int(start[1]))
 	goal = (int(goal[0]), int(goal[1]))
 	neighbors = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
@@ -18,7 +19,7 @@ def astar(array, start, goal):
 	fscore = {start:heuristic(start, goal)}
 	oheap = []
 	heappush(oheap, (fscore[start], start))
-	
+
 	while oheap:
 
 		current = heappop(oheap)[1]
@@ -32,7 +33,7 @@ def astar(array, start, goal):
 
 		close_set.add(current)
 		for i, j in neighbors:
-			neighbor = current[0] + i, current[1] + j            
+			neighbor = current[0] + i, current[1] + j
 			tentative_g_score = gscore[current] + heuristic(current, neighbor)
 			if 0 <= neighbor[0] < array.shape[0]:
 				if 0 <= neighbor[1] < array.shape[1]:
@@ -44,27 +45,27 @@ def astar(array, start, goal):
 			else:
                 # array bound x walls
 				continue
-                
+
 			if neighbor in close_set and tentative_g_score >= gscore.get(neighbor, 0):
 				continue
-                
+
 			if  tentative_g_score < gscore.get(neighbor, 0) or neighbor not in [i[1]for i in oheap]:
 				came_from[neighbor] = current
 				gscore[neighbor] = tentative_g_score
 				fscore[neighbor] = tentative_g_score + heuristic(neighbor, goal)
 				heappush(oheap, (fscore[neighbor], neighbor))
-				
+
 	return list(range(0,9999))
-    
+
 def calc_path(img, X, Y):
 	path = []
 	for x in X:
 		for y in Y:
 			data = astar(img, tuple(x), tuple(y))
-			
+
 			path.append(len(data))
-	
-	
+
+
 	return np.array(path)
 
 #init
@@ -86,15 +87,15 @@ def init_kmean(K, X, img):
 
                 s = 9999
                 label = 0
-                for i in mean:                
+                for i in mean:
                         length = len(astar(img, x, X[i]))
-                        print(cnt, s, length)
+
                         if s > length:
                                 s = length
                                 label = i
-                                
+
                 X_label.append(mean.index(label))
-                
+
                 cnt += 1
 
         return X_label
@@ -122,7 +123,7 @@ def calc_G(K, X, img, X_label):
 
 
 def is_wall(img, p):
-        print(type(p), p)
+
         row, col = p
 
         row = int(row)
@@ -147,47 +148,92 @@ def is_wall(img, p):
                     return np.array([row-flag, col])
         else:
             return np.array([row, col])
-K = 3
-img = cv2.imread('newmap.png', 2)
-#X = np.array([[[40,1], [42, 10]], [[10, 34], [21,16], [2, 23]], [[37, 26], [39, 37], [37, 41], [47, 48], [48,12]]])
-#X = np.array([[40,1], [42, 10], [10, 34], [21,16], [2, 23], [37, 26], [39, 37], [37, 41], [47, 48], [48,12]])
-X = np.array([[1, 6], [1, 23], [2, 2], [2, 12], [2, 16], [2, 17], [3, 6], [3, 28], [3, 31], [4, 36], [5, 15], [5, 36], [6, 5], [6, 9], [6, 19], [6, 22], [9, 3], [9, 12], [9, 16], [10, 3], [10, 36], [11, 22], [11, 27], [11, 34], [12, 27], [12, 34], [13, 3], [13, 6], [13, 7], [13, 17], [13, 20], [14, 12], [14, 25], [14, 34], [15, 31], [15, 34], [16, 15], [16, 20], [17, 7], [17, 10], [17, 22], [18, 2], [19, 10], [19, 30], [19, 33], [19, 34], [19, 38], [20, 5], [21, 7], [21, 10], [21, 14], [21, 16], [21, 17], [21, 19], [21, 20], [21, 27], [22, 10], [22, 32], [23, 2], [23, 5], [23, 7]])
-X_label = init_kmean(K, X, img)
-mean = calc_G(K, X, img, X_label)
 
-print(mean)
-print(X_label)
-# print("check")
-#run kmean
-while True:
-        temp_label = []
-        cnt= 0
-        for x in X:
+def write_to_img(img_name, X, X_label):
 
-                s = 9999
-                label = 0
+        img = cv2.imread(img_name)
 
-                for i in range(len(mean)):                
-                        length = len(astar(img, x, list(mean[i])))
-                        print(cnt, s, length)
-                        if s > length:
-                                s = length
-                                label = i
-                                
-                temp_label.append(label)
-                cnt+=1
-                
-        print(temp_label)
-        if X_label == temp_label:
-                break
-        X_label = copy.deepcopy(temp_label)
-        mean = calc_G(K, X, img, temp_label)
-        print(X_label)
-        print(mean)
+        color_list = []
 
-cnt = 0
-for i, j in X:        
-        img[i][j] = int((255/(X_label[cnt]+1))+10)
-        cnt+=1
-        
-cv2.imwrite('result.png', img)
+        while len(color_list) <= max(X_label)+1:
+
+                color = list(np.random.random(size=3) * 255)
+
+                while color in color_list:
+                        color = list(np.random.random(size=3) * 255)
+
+                color_list.append(color)
+
+        for i in range(len(X)):
+
+                row = X[i][0]
+                col = X[i][1]
+
+                img[row][col] = color_list[X_label[i]]
+        '''
+        cv2.imshow('result', img)
+
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        '''
+
+        cv2.imwrite('result.png', img)
+
+def run_kmeans(K, X, img_name):
+    img = cv2.imread(img_name, 2)
+    #X = np.array([[[40,1], [42, 10]], [[10, 34], [21,16], [2, 23]], [[37, 26], [39, 37], [37, 41], [47, 48], [48,12]]])
+    #X = np.array([[40,1], [42, 10], [10, 34], [21,16], [2, 23], [37, 26], [39, 37], [37, 41], [47, 48], [48,12]])
+
+    X_label = init_kmean(K, X, img)
+    mean = calc_G(K, X, img, X_label)
+
+    # print("check")
+    #run kmean
+    while True:
+            temp_label = []
+            density = []
+
+            for i in range(K):
+                density.append(0)
+
+            cnt= 0
+            for x in X:
+
+                    s = 9999
+                    label = 0
+
+                    for i in range(len(mean)):
+                            length = len(astar(img, x, list(mean[i])))
+
+                            if s > length:
+                                    s = length
+                                    label = i
+                    density[label] += s
+                    temp_label.append(label)
+                    cnt+=1
+
+
+            if X_label == temp_label:
+                    break
+            X_label = copy.deepcopy(temp_label)
+            mean = calc_G(K, X, img, temp_label)
+
+    return X_label, density
+
+if __name__ == '__main__':
+
+    elbow = []
+
+    X = np.array([[1, 6], [1, 23], [2, 2], [2, 12], [2, 16], [2, 17], [3, 6], [3, 28], [3, 31], [4, 36], [5, 15], [5, 36], [6, 5], [6, 9], [6, 19], [6, 22], [9, 3], [9, 12], [9, 16], [10, 3], [10, 36], [11, 22], [11, 27], [11, 34], [12, 27], [12, 34], [13, 3], [13, 6], [13, 7], [13, 17], [13, 20], [14, 12], [14, 25], [14, 34], [15, 31], [15, 34], [16, 15], [16, 20], [17, 7], [17, 10], [17, 22], [18, 2], [19, 10], [19, 30], [19, 33], [19, 34], [19, 38], [20, 5], [21, 7], [21, 10], [21, 14], [21, 16], [21, 17], [21, 19], [21, 20], [21, 27], [22, 10], [22, 32], [23, 2], [23, 5], [23, 7]])
+
+    for i in range(1, 10):
+        X_label, density = run_kmeans(i, X, 'newmap.png')
+        elbow.append(sum(density))
+
+    x = range(1, 10)
+    y = elbow
+
+    plt.plot(x, y)
+    plt.show()
+
+    write_to_img('newmap.png', X, X_label)
