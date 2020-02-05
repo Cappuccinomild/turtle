6033e3837b188803d081f1610cc0580685210183
SONGEUN <songeuny98@gmail.com>
2020-02-05 21:10:37 +0900

    Refactoring build_map.py

--- build_map.py
+++ build_map.py
@@ -1,17 +1,23 @@
 import cv2
 
-turtlebot_radius=3
-img = cv2.imread('origin_map.png', 2)
-direction=[[i,j] for i in range(-2,3) for j in range(-2,3)]
-direction.remove([0,0])
-direction.extend([[3,0],[-3,0],[0,3],[0,-3]])
+turtle_bot_radius = 3
+wall = 0
+road = 255
+gray_zone = 195
+origin_map_name = 'origin_map.png'
+build_map_name = 'built_map.png'
+
+img = cv2.imread(origin_map_name, 2)
+direction = [[i, j] for i in range(- turtle_bot_radius + 1, turtle_bot_radius) for j in range(-turtle_bot_radius + 1, turtle_bot_radius)]
+direction.remove([0, 0])
+direction.extend([[turtle_bot_radius, 0], [-turtle_bot_radius, 0], [0, turtle_bot_radius], [0, -turtle_bot_radius]])
 for i in range(img.shape[0]):
     for j in range(img.shape[1]):
-        if img[i][j]==0: #현재가 검정색일 때
-            for k,l in direction: #상하좌우 빈곳 탐지
-                if ((i+k)<0 or (i+k)>=img.shape[0]) or ((j+l)<0 or (j+l)>=img.shape[1]):
+        if img[i][j] == wall:
+            for k, l in direction:
+                if ((i + k) < wall or (i + k) >= img.shape[0]) or ((j + l) < wall or (j + l) >= img.shape[1]):
                     continue
-                if (img[i+k][j+l] != 255) and (img[i+k][j+l] != 120):
+                if (img[i + k][j + l] != road) and (img[i + k][j + l] != gray_zone):
                     continue
-                img[i+k][j+l]=120
-cv2.imwrite('build_map2.png', img)
+                img[i+k][j+l] = gray_zone
+cv2.imwrite(build_map_name, img)
